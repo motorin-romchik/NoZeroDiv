cd1fb94a4c59333fa4db7f96ca3404f0dcc523eb
mr_str <80831520+motorin-romchik@users.noreply.github.com>
2022-03-23 00:06:46 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main (String args[]){
+        int result;
+
+        for (int i = -5; i <6; i++){
+            result = i  != 0 ? 100/i : 0;
+            if (i!=0)
+                System.out.println("100 / "+ i + " равно "+ result);
+        }
+    }
+}
